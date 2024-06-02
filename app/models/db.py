9d17a478e37bc4ee3454e326f5ac84872dacd115
angelwayar <angelmanuelwayar@gmail.com>
2024-06-02 11:01:15 -0400

    update User table

--- app/models/db.py
+++ app/models/db.py
@@ -6,11 +6,20 @@ from sqlalchemy import ForeignKey
 
 from sqlalchemy.ext.declarative import declarative_base
 
+from models.dto import GetUser
+
 Base = declarative_base()
 
 
 class User(Base):
     __tablename__ = 'users'
+
     id = Column("id", Integer(), primary_key=True, autoincrement=True)
     email = Column("email", String(256), unique=True)
     password = Column("password", String(256))
+
+    def to_GetUser(self) -> GetUser:
+        return GetUser(
+            id=self.id,
+            email=self.email
+        )
