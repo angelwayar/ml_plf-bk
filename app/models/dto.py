2bf9dee8ba7bc3bbb2705f376e0d3d39c3a8247d
angelwayar <angelmanuelwayar@gmail.com>
2024-06-02 09:58:54 -0400

    update many files

--- app/models/dto.py
+++ app/models/dto.py
@@ -2,28 +2,45 @@ from datetime import datetime
 from pydantic import BaseModel
 
 # USER
+
+
 class CreateUser(BaseModel):
     email: str
     password: str
+
 
 class GetUser(BaseModel):
     id: int
     email: str
 
+
 class UpdateUser(BaseModel):
     email: str
 
 # Image
+
+
 class CreateImage(BaseModel):
     name: str
     location: str
+
 
 class GetImage(BaseModel):
     id: int
     name: str
     location: str
 
+# LOGIN
+
+
+class LoginUser(BaseModel):
+    email: str
+    password: str
+
+
 # TOKEN
+
+
 class Token(BaseModel):
     id: int
     user_id: int
---
+++ app/utils/formating.py
@@ -0,0 +1,2 @@
+def format_string(email: str) -> str:
+    return email.strip().replace(" ", "").replace("\n", "").replace("\r", "").lower()
