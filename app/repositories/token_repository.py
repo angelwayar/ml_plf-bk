a209c9c73231d74a0b872cc3d547ee77e26b443a
angelwayar <angelmanuelwayar@gmail.com>
2024-06-11 20:22:48 -0400

    comment all token_repository and update token_service

--- app/repositories/token_repository.py
+++ app/repositories/token_repository.py
@@ -1,57 +1,57 @@
-from datetime import datetime
+# from datetime import datetime
 
-from sqlalchemy import Delete
+# from sqlalchemy import Delete
 
-from models.db import Token, TokenEntity
-from db.context import session_maker
-
-
-def add(user_id: int, hash: str, valid_to: datetime) -> TokenEntity:
-    with session_maker.begin() as session:
-        token = Token()
-        token.user_id = user_id
-        token.hash = hash
-        token.expired_at = valid_to
-
-        session.add(token)
-        session.flush()
-
-        return token.to_TokenEntity()
+# from models.db import TokenEntity
+# from db.context import session_maker
 
 
-def delete(id: int) -> None:
-    with session_maker.begin() as session:
-        session.execute(Delete(Token).where(Token.id == id))
+# def add(user_id: int, hash: str, valid_to: datetime) -> TokenEntity:
+#     with session_maker.begin() as session:
+#         token = Token()
+#         token.user_id = user_id
+#         token.hash = hash
+#         token.expired_at = valid_to
+
+#         session.add(token)
+#         session.flush()
+
+#         return token.to_TokenEntity()
 
 
-def delete_by_user_id(id: int) -> None:
-    with session_maker.begin() as session:
-        session.execute(Delete(Token).where(Token.user_id == id))
+# def delete(id: int) -> None:
+#     with session_maker.begin() as session:
+#         session.execute(Delete(Token).where(Token.id == id))
 
 
-def get_by_id(id: int) -> TokenEntity | None:
-    with session_maker() as session:
-        return session.query(Token).where(
-            Token.id == id
-        ).first().to_TokenEntity()
+# def delete_by_user_id(id: int) -> None:
+#     with session_maker.begin() as session:
+#         session.execute(Delete(Token).where(Token.user_id == id))
 
 
-def get_by_user_id(user_id: int, limit: int = 1000, offset: int = 0) -> list[TokenEntity]:
-    list_TokenEntity = []
-
-    with session_maker() as session:
-        list_token = session.query(Token).where(
-            Token.user_id == user_id
-        ).limit(limit).offset(offset).all()
-
-        for token in list_token:
-            list_TokenEntity.append(token.to_TokenEntity())
-
-        return list_TokenEntity
+# def get_by_id(id: int) -> TokenEntity | None:
+#     with session_maker() as session:
+#         return session.query(Token).where(
+#             Token.id == id
+#         ).first().to_TokenEntity()
 
 
-def get_by_hash(obj_hash: str) -> TokenEntity | None:
-    with session_maker() as session:
-        return session.query(Token).where(
-            Token.hash == obj_hash
-        ).first().to_TokenEntity()
+# def get_by_user_id(user_id: int, limit: int = 1000, offset: int = 0) -> list[TokenEntity]:
+#     list_TokenEntity = []
+
+#     with session_maker() as session:
+#         list_token = session.query(Token).where(
+#             Token.user_id == user_id
+#         ).limit(limit).offset(offset).all()
+
+#         for token in list_token:
+#             list_TokenEntity.append(token.to_TokenEntity())
+
+#         return list_TokenEntity
+
+
+# def get_by_hash(obj_hash: str) -> TokenEntity | None:
+#     with session_maker() as session:
+#         return session.query(Token).where(
+#             Token.hash == obj_hash
+#         ).first().to_TokenEntity()
--- app/services/token_service.py
+++ app/services/token_service.py
@@ -3,7 +3,7 @@ from datetime import timezone
 from datetime import timedelta
 
 from models import db
-from repositories import token_repository
+# from repositories import token_repository
 from utils import hashing
 
 SESSION_TTL = timedelta(days=7)
@@ -34,19 +34,19 @@ def create(user_id: int) -> db.TokenEntity:
 #     return token_repository.get_by_hash(hash)
 
 
-def delete(token_id: int) -> None:
-    token_repository.delete(token_id)
+# def delete(token_id: int) -> None:
+#     token_repository.delete(token_id)
 
 
-def delete_by_hash(hash: str) -> None:
-    token = get_by_hash(hash)
-    if token:
-        delete(token.id)
+# def delete_by_hash(hash: str) -> None:
+#     token = get_by_hash(hash)
+#     if token:
+#         delete(token.id)
 
 
-def delete_expired(user_id: int) -> None:
-    tokens = token_repository.get_by_user_id(user_id)
-    now = datetime.now(timezone.utc).replace(tzinfo=None)
-    for t in tokens:
-        if t.expired_at < now:
-            delete(t)
+# def delete_expired(user_id: int) -> None:
+#     tokens = token_repository.get_by_user_id(user_id)
+#     now = datetime.now(timezone.utc).replace(tzinfo=None)
+#     for t in tokens:
+#         if t.expired_at < now:
+#             delete(t)
