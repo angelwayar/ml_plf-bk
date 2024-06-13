fdf8a5917e39893aff64594f01d5e332319b5c88
angelwayar <angelmanuelwayar@gmail.com>
2024-06-13 15:45:13 -0400

    update middleware

--- app/core/middleware/auth_middleware.py
+++ app/core/middleware/auth_middleware.py
@@ -3,14 +3,16 @@ from datetime import datetime
 from fastapi import HTTPException, Request, status
 from models import dto
 from services.token_jwt_service import parse
+from services.user_service import get_by_id
 
 
 async def auth_middleware(req: Request):
     try:
         token = req.headers["bearerToken"]
         object_token: dto.Token = parse(token=token)
-
-        if object_token.expired_at < datetime.now():
+        user = await get_by_id(id=object_token.user_id)
+        
+        if object_token.expired_at < datetime.now() and user is None:
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
                 detail="Token expired"
